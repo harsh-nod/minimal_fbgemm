1be8958121b2f9081001fce8274190c65756eaca
Harsh Menon <harsh@nod-labs.com>
2025-09-17 08:50:52 -0700

    Modify access of sorted_infos to get kernel running

--- src/gen_embedding_backward_split_unweighted_device_kernel.cuh
+++ src/gen_embedding_backward_split_unweighted_device_kernel.cuh
@@ -1,3 +1,11 @@
+////////////////////////////////////////////////////////////////////////////////
+// GENERATED FILE INFO
+//
+// Template Source: training/backward/embedding_backward_split_device_kernel_template.cuh
+////////////////////////////////////////////////////////////////////////////////
+
+
+
 /*
  * Copyright (c) Meta Platforms, Inc. and affiliates.
  * All rights reserved.
@@ -6,7 +14,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#pragma once
+// clang-format off
 
 #include "fbgemm_gpu/embedding_backward_template_helpers.cuh"
 #include "fbgemm_gpu/utils/tensor_accessor_builder.h"
@@ -38,31 +46,85 @@ DEVICE_INLINE void compute_grad_sum_unweighted(
     const unsigned int shfl_sync_mask,
     const int32_t num_vecs
 ) {
-    // Simplified implementation for testing
     // Copy value to vecs to make num_vecs known at compile time when
     // kUseVecBlocking == false
-    const int32_t max_vecs =
-        kUseVecBlocking ? num_vecs : kFixedMaxVecsPerThread;
+    const int32_t vecs = kUseVecBlocking ? num_vecs : kFixedMaxVecsPerThread;
+    for (int32_t vec_start = 0;
+         vec_start < vecs;
+         vec_start += kFixedMaxVecsPerThread) {
 
-    // Basic gradient sum computation
-    for (int32_t d = 0; d < D; d += VEC_WIDTH) {
-        Vec4TAcc<cache_t> grad_vec;
-        grad_vec.clear();
-
-        // Accumulate gradients
-        for (int32_t i = sl_start; i < sl_end; ++i) {
-            const int32_t info = sorted_infos[i];
-            const int32_t t = info >> info_B_num_bits;
-            const int32_t b = info & info_B_mask;
-
-            if (d + VEC_WIDTH <= D) {
-                grad_vec.acc.x += grad_output[b][D_offsets[t] + d];
-                if (VEC_WIDTH > 1) grad_vec.acc.y += grad_output[b][D_offsets[t] + d + 1];
-                if (VEC_WIDTH > 2) grad_vec.acc.z += grad_output[b][D_offsets[t] + d + 2];
-                if (VEC_WIDTH > 3) grad_vec.acc.w += grad_output[b][D_offsets[t] + d + 3];
-            }
+        // Reset grad_sum vectors
+        #pragma unroll kFixedMaxVecsPerThread
+        for (int32_t vec = 0; vec < kFixedMaxVecsPerThread; vec++) {
+            grad_sum[vec].acc.x = 0;
+            grad_sum[vec].acc.y = 0;
+            grad_sum[vec].acc.z = 0;
+            grad_sum[vec].acc.w = 0;
         }
 
-        grad_sum[d / VEC_WIDTH] = grad_vec;
+        // ------------------ HOT LOOP --------------------------//
+        for (int32_t sl = sl_start; sl < sl_end; sl += kThreadGroupSize) {
+            auto sl_j = sl + threadIdx.x;
+            const bool full_tile = sl + kThreadGroupSize <= sl_end;
+            // Go down the fast path if all threads are within the bounds.
+            if (full_tile) {
+                const auto b_t = reinterpret_cast<const uint32_t*>(&sorted_infos[0])[0];
+                const auto b = b_t & info_B_mask;
+                const auto t = b_t >> info_B_num_bits; // if vbe
+                int32_t D_start = D_offsets[t]; // if vbe // if not nobag
+
+                #pragma unroll 64
+                for (int32_t j = 0; j < kThreadGroupSize; ++j) {
+                    int32_t b_j = SHFL_SYNC(b, j);
+                    int32_t D_start_j = SHFL_SYNC(D_start, j);
+
+                    #pragma unroll kFixedMaxVecsPerThread
+                    for (int32_t vec = 0; vec < kFixedMaxVecsPerThread; ++vec) {
+                        const int32_t d = (((vec + vec_start) * kThreadGroupSize + threadIdx.x) * VEC_WIDTH);
+                        if (d < D) {
+                            Vec4TAcc<grad_t> grad_out_vec(
+                                &grad_output[b_j][0] + D_start_j + d // if nobag
+                            );
+                            grad_sum[vec].add_(grad_out_vec);
+                        }
+                    }
+                }
+              continue;
+            }
+            const auto b_t = sl_j < sl_end
+                ? reinterpret_cast<const uint32_t*>(
+                    &sorted_infos[0])[0]
+                : 0;
+            const auto b = b_t & info_B_mask;
+            const auto t = b_t >> info_B_num_bits; // if vbe
+            int32_t D_start = sl_j < sl_end ? D_offsets[t] : 0; // if vbe // if not nobag
+            for (int32_t j = 0; j < kThreadGroupSize && sl + j < sl_end; ++j) {
+                int32_t b_j = SHFL_SYNC(b, j);
+                int32_t D_start_j = SHFL_SYNC(D_start, j);
+
+                #pragma unroll kFixedMaxVecsPerThread
+                for (int32_t vec = 0; vec < kFixedMaxVecsPerThread && (((vec + vec_start) * kThreadGroupSize + threadIdx.x) * VEC_WIDTH) < D; ++vec) {
+                    const int32_t d = (((vec + vec_start) * kThreadGroupSize + threadIdx.x) * VEC_WIDTH);
+                    Vec4TAcc<grad_t> grad_out_vec(
+                        &grad_output[b_j][0] + D_start_j + d // if nobag
+                    );
+                    grad_sum[vec].add_(grad_out_vec);
+                }
+            }
+        }
+        // ------------------ HOT LOOP END --------------------------//
+
+        if (smem_grad_sum) {
+            // Store grad_sum in smem_grad_sum
+            #pragma unroll kFixedMaxVecsPerThread
+            for (int32_t vec = 0;
+                 (vec < kFixedMaxVecsPerThread) && ((vec + vec_start) * kThreadGroupSize + threadIdx.x) * VEC_WIDTH < D;
+                 ++vec) {
+                const int32_t d_vec = ((vec + vec_start) * kThreadGroupSize + threadIdx.x);
+                smem_grad_sum[d_vec] = grad_sum[vec];
+            }
+        }
     }
 }
+
+    // clang-format on
